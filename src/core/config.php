55a56ffe4f517eadaea31fcf31e325e91eaee63c
MikeRider27 <mike.mavc27@gmail.com>
2026-04-27 12:38:22 -0300

    Fix variables para conectar a base de datos - para implementar a futuro

--- src/core/config.php
+++ src/core/config.php
@@ -2,5 +2,12 @@
 // Definir la zona horaria predeterminada
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 
+// Definimos las variables de conexión capacitacion
+define("DB_HOST", "localhost");
+define("DB_PORT", "5432");
+define("DB_NAME", "bd_name");
+define("DB_USER", "user_name");
+define("DB_PASSWORD", "password");
+
 //FHIR SERVER ENDPOINT
 define("APP_FHIR_SERVER", "https://fhir-conectaton.mspbs.gov.py/fhir");
